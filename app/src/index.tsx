751778326193c217d29e7ad33c2e9f1de42d74ff
Jeff <antunis356@gmail.com>
2020-11-12 16:02:12 -0300

    Adicionado OneSignal no aplicativo

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,8 +1,10 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+import OneSignal from 'react-native-onesignal';
 
 import SignIn from './pages/SignIn';
 import Home from './pages/Home';
@@ -21,7 +23,23 @@ const StackApp = () => (
   </Stack.Navigator>
 );
 
+const onReceived = (notification: any) => {
+  console.log('Notification received: ', notification);
+};
+
+const onOpened = (openResult: any) => {
+  console.log('Message: ', openResult.notification.payload.body);
+  console.log('Data: ', openResult.notification.payload.additionalData);
+  console.log('isActive: ', openResult.notification.isAppInFocus);
+};
+
 const App = () => {
+  useEffect(() => {
+    OneSignal.init('230b5cf4-6dbc-4ab0-8b33-3cc56469976f');
+    OneSignal.addEventListener('received', onReceived);
+    OneSignal.addEventListener('opened', onOpened);
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#202020" />
